2a21c272bdbb8338783c65b1e21745e4dd38b592
Miillky <vedran.milkovic25@gmail.com>
2020-05-30 22:22:57 +0200

    Added constants

---
+++ src/JFX/Constants.java
@@ -0,0 +1,23 @@
+package JFX;
+
+public class Constants {
+
+    public static final String MAIN_EXCEL_SHEET_NAME = "Radnici";
+    public static final String SECONDARY_EXCEL_SHEET_NAME = "Koeficijenti";
+
+    public static final String TABLE_CONTENT_TEXT = "Odaberite datoteku i generirajte listu zaposlenika";
+
+    public static final String CHOSE_FILE_WINDOW_TITLE = "Odaberite datoteku";
+    public static final String EXCEL_FILE_ERROR_TITLE = "Greška - Pogrešna datoteka!";
+    public static final String EXCEL_FILE_ERROR_MESSAGE = "Niste odabrali xlsx datoteku!";
+    public static final String EXCEL_ROW_ERROR_TITLE = "Greška - Prazan redak";
+    public static final String EXCEL_ROW_ERROR_MESSAGE = "Jedan ili više retka su prazni, provjerite datoteku!";
+    public static final String EXCEL_COLUMN_ERROR_TITLE = "Greška - Prazan stupac";
+    public static final String EXCEL_COLUMN_ERROR_MESSAGE = "Jedan ili više supaca su prazni, provjerite datoteku!";
+    public static final String EMPLOYEE_ERROR_TITLE = "Greška - Odaberite zaposlenika";
+    public static final String EMPLOYEE_ERROR_MESSAGE = "Odaberite jednog ili više zaposlenika da bi generirali PDF datoteku!";
+    public static final String EMPLOYEE_SUCCESS_TITLE = "Datoteka generiran";
+    public static final String EMPLOYEE_SUCCESS_MESSAGE = "PDF datoteka je generirana!";
+    public static final String EMPLOYEES_SUCCESS_TITLE = "Datoteke generirane";
+    public static final String EMPLOYEES_SUCCESS_MESSAGE = "PDF datoteke su generirane!";
+}
